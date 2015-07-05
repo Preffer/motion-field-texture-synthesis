2411e9ae89b0196f8b4b0226b6b81afa4689e209
Yuzo <hyzgog@gmail.com>
2015-07-05 14:04:24 +0800

    tweak para

--- cvRender/main.cpp
+++ cvRender/main.cpp
@@ -18,7 +18,7 @@ float BORN_BASE_ROW = ROWS / 2;
 float BORN_VARY_COL = 0;
 float BORN_VARY_ROW = ROWS;
 
-float SPEED_COL = -3;
+float SPEED_COL = -4;
 float SPEED_ROW = 0;
 
 float MOTION_BASE_COL = 128;
@@ -31,22 +31,33 @@ int FORCE_COL = 420;
 int FORCE_ROW = 240;
 int FORCE_RANGE = 108;
 
-int BORN_SPEED = 10;
+int BORN_SPEED = 20;
 float MIN_SPEED = 0.2f;
 
-int SWIRL_MIN_COL = 100;
-int SWIRL_MAX_COL = 400;
-int SWIRL_BASE_COL = 400;
-int SWIRL_VARY_COL = 0;
+int SWIRL_TOP_COL = 0;
+int SWIRL_TOP_ROW = 100;
 
-int SWIRL_MIN_ROW = 100;
-int SWIRL_MAX_ROW = 400;
-int SWIRL_BASE_ROW = 250;
-int SWIRL_VARY_ROW = 150;
+int SWIRL_RIGHT_COL = 240;
+int SWIRL_RIGHT_ROW = 240;
+
+int SWIRL_BOTTOM_COL = ROWS - 1;
+int SWIRL_BOTTOM_ROW = 100;
+
+int SWIRL_BASE_COL = 240;
+int SWIRL_BASE_ROW = 400;
+
+int SWIRL_VARY_COL = 60;
+int SWIRL_VARY_ROW = 40;
+
+float SWIRL_LINE1_K = 1.1;
+float SWIRL_LINE1_B = -100;
+
+float SWIRL_LINE2_K = -1.1;
+float SWIRL_LINE2_B = 580;
 
 float SWIRL_DIS = 100.0;
-float SWIRL_BORN_SPEED = 0.1;
-float SWIRL_RAD = 0.01;
+float SWIRL_BORN_INTERVAL = 24;
+float SWIRL_RAD = 0.001;
 
 class Particle {
 public:
@@ -66,15 +77,12 @@ Particle::Particle() {
 
 class Swirl {
 public:
-	Swirl();
+	Swirl(float row, float col);
 	float row;
 	float col;
 };
 
-Swirl::Swirl() {
-	col = SWIRL_BASE_COL + (float(rand()) / RAND_MAX - 0.5) * SWIRL_VARY_COL;
-	row = SWIRL_BASE_ROW + (float(rand()) / RAND_MAX - 0.5) * SWIRL_VARY_ROW;
-}
+Swirl::Swirl(float row, float col) : row(row), col(col) {}
 
 int main(int argc, char* argv[]) {
 	Mat motion = imread("plast.bmp", CV_LOAD_IMAGE_COLOR);
@@ -87,16 +95,21 @@ int main(int argc, char* argv[]) {
 	imshow("Scene", motion);
 	//createTrackbar("ROW_SWIRL", "Scene", &ROW_SWIRL, ROWS);
 	//createTrackbar("COL_SWIRL", "Scene", &COL_SWIRL, COLS);
-	//swirls.push_back(Swirl());
 
+	int frame = 0;
 	while (true) {
 		for (int i = 0; i < BORN_SPEED; i++) {
 			particles.push_back(Particle());
 		}
-		if ((float(rand()) / RAND_MAX) < 0.05) {
-			swirls.push_back(Swirl());
+		if (frame++ == 0) {
+			for (int i = -4; i < 8; i++) {
+				swirls.push_back(Swirl(SWIRL_BASE_COL + i * SWIRL_VARY_COL, SWIRL_BASE_ROW + i * SWIRL_VARY_ROW));
+			}
+		} else {
+			if (frame == SWIRL_BORN_INTERVAL) {
+				frame = 0;
+			}
 		}
-
 		for (auto s = swirls.begin(); s != swirls.end(); s++) {
 			if (s->col < 0 || s->col >= COLS || s->row < 0 || s->row >= ROWS) {
 				swirls.erase(s--);
@@ -112,7 +125,7 @@ int main(int argc, char* argv[]) {
 		for (auto p = particles.begin(); p != particles.end(); p++) {
 			Vec3b bgrPixel = motion.at<Vec3b>(p->row, p->col);
 
-			if (p->row > SWIRL_MIN_ROW && p->row < SWIRL_MAX_ROW && p->col > SWIRL_MIN_COL && p->col < SWIRL_MAX_COL) {
+			if ((p->col * SWIRL_LINE1_K + SWIRL_LINE1_B < p->row) && (p->col * SWIRL_LINE2_K + SWIRL_LINE2_B > p->row)) {
 				float swirlCol = swirls.front().col;
 				float swirlRow = swirls.front().row;
 				float colVector = p->col - swirlCol;
@@ -134,7 +147,7 @@ int main(int argc, char* argv[]) {
 				float vectorAngle = atan2f(rowVector, colVector);
 
 				//cout << boost::format("%1%: %2%") % vectorDis % vectorAngle << endl;
-				float swirlAngle = 1e15 / powf(vectorDis, 8) * SWIRL_RAD;
+				float swirlAngle = 1e8 / powf(vectorDis, 4) * SWIRL_RAD;
 				if (swirlAngle > 0.05) {
 					swirlAngle = 0.05;
 				}
@@ -172,10 +185,6 @@ int main(int argc, char* argv[]) {
 				//p->rowSpeed = speedDis * sinf(newAngle);
 
 			} else {
-				/*p->colSpeed = (SPEED_COL + p->colSpeed * 9) / 10;
-				p->rowSpeed = (SPEED_ROW + p->rowSpeed * 9) / 10;
-				p->col += p->colSpeed;
-				p->row += p->rowSpeed;*/
 				p->col += SPEED_COL;
 				p->row += SPEED_ROW;
 				p->colSpeed = SPEED_COL / 3;
@@ -191,11 +200,11 @@ int main(int argc, char* argv[]) {
 
 		}
 
-		for (auto s : swirls) {
+		/*for (auto s : swirls) {
 			circle(scene, Point(s.col, s.row), 10, CV_RGB(175, 0, 0), 2);
-		}
+		}*/
 		
-		//circle(scene, Point(FORCE_COL, FORCE_ROW), FORCE_RANGE, CV_RGB(175, 0, 0), 2);
+		circle(scene, Point(FORCE_COL, FORCE_ROW), FORCE_RANGE, CV_RGB(175, 0, 0), 2);
 		imshow("Scene", scene);
 		waitKey(1);
 	}
